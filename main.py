ca0c64f4650bbd51b2f4efa90db1d9ec95a4d1db
HeckerWithAHat <aryanj1010@gmail.com>
2024-05-16 15:57:01 -0700

    using flask, and added python to lang list

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template, send_file
+
+app = Flask(__name__)
+banned_files = ["main.py"]
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/languages")
+def languages():
+    return render_template("languages.html")
+
+
+@app.route("/<path:name>")
+def get_file(name):
+    try:
+        if name in banned_files:
+            return " "
+        else:
+            return send_file(name)
+    except:
+        return " "
+
+if __name__ == "__main__":
+    app.run(debug="true")
